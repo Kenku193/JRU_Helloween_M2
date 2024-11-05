1e57917361f000c4518522db3034caad5f0c45ea
mentor <kenku@mail.ru>
2024-11-05 13:04:19 +0300

    Module 2 Level 14 j.u.c.

---
+++ src/level14_juc/Example4/C.java
@@ -0,0 +1,27 @@
+package level14_juc.Example4;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class C {
+    public static void main(String[] args) {
+
+
+        /*
+LinkedList - ConcurrentLinkedQueue
+
+ArrayDeque - ConcurrentLinkedDeque
+
+ArrayList - CopyOnWriteArrayList
+
+HashSet - CopyOnWriteArraySet
+
+HashMap - ConcurrentHashMap
+
+TreeMap - ConcurrentSkipListMap
+
+TreeSet - ConcurrentSkipListSet
+        * */
+
+
+    }
+}
